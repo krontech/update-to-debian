64b698cb815dcc269a86a24e639929183b84c64b
Simon Kronstein <skronstein7@gmail.com>
2018-10-31 19:39:31 -0700

    Add more missing lines and files

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "updatewindow.h"
+#include <QWSServer>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+	QApplication app(argc, argv);
+	QWSServer::setCursorVisible(false);
+	
 	UpdateWindow w;
 	w.show();
 	
-	return a.exec();
+	return app.exec();
 }
