f02612dd07c062ab1ed5a1d8127850a092dfe4e7
Simon Kronstein <skronstein7@gmail.com>
2018-11-14 19:32:58 -0800

    add: update.sh

---
+++ update.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+# Script to perform software update for Chronos 1.4 camera
+#
+# Authors: David Kronstein, Kron Technologies Inc.
+#          Matthew Peters, Kron Technologies Inc.
+#          Simon Kronstein, Kron Technologies Inc.
+#
+
+sh $(dirname $0)/update_real.sh &
