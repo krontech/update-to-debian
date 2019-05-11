8347554fb9241b0bdbe808d6316def8ce681f186
Simon Kronstein <skronstein7@gmail.com>
2019-05-10 20:39:42 -0700

    Fix running from USB
    FAT32 does not support FIFOs, and this app requires FIFOs, so the app was broken when run from USB.
    Fix this by by creating the FIFOs in /opt/camera instead of in the same directory as the scripts.

--- update_real.sh
+++ update_real.sh
@@ -3,15 +3,15 @@
 cd $(realpath $(dirname $0))
 
 #May fail, probably because they already exist. That's OK.
-mkfifo to-update-ui || true
-mkfifo to-update-backend || true
+mkfifo /opt/camera/to-update-ui || true
+mkfifo /opt/camera/to-update-backend || true
 
 >&2 echo "stopping camApp"
 killall camApp || true
 sleep 0.8
 
 >&2 echo "starting update-to-debian"
-./update_backend.sh < to-update-backend > to-update-ui &
-./update-to-debian -qws > to-update-backend < to-update-ui &
+./update_backend.sh < /opt/camera/to-update-backend > /opt/camera/to-update-ui &
+./update-to-debian -qws > /opt/camera/to-update-backend < /opt/camera/to-update-ui &
 
 wait
