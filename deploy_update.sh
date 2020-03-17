c9fa4faaca2c96aca3bbeab99b945e032fe86f66
Simon Kronstein <skronstein7@gmail.com>
2020-03-16 17:36:47 -0700

    Fix for ubuntu 18.04 - use scp1 instead of scp

--- deploy_update.sh
+++ deploy_update.sh
@@ -1,9 +1,9 @@
 DEPLOYDIR=/opt/camera/camUpdate
 echo deploydir is $DEPLOYDIR
 echo working dir: $PWD
-scp -oKexAlgorithms=+diffie-hellman-group1-sha1  "camUpdate/update_real.sh"  root@192.168.12.1:$DEPLOYDIR/;
-scp -oKexAlgorithms=+diffie-hellman-group1-sha1  "camUpdate/update_backend.sh"  root@192.168.12.1:$DEPLOYDIR/;
-scp -oKexAlgorithms=+diffie-hellman-group1-sha1  "camUpdate/update.md5sum"  root@192.168.12.1:$DEPLOYDIR/;
-scp -oKexAlgorithms=+diffie-hellman-group1-sha1  "camUpdate/update.sh"  root@192.168.12.1:$DEPLOYDIR/;
-scp -oKexAlgorithms=+diffie-hellman-group1-sha1  "camUpdate/Chronos1_4PowerController.X.production.hex"  root@192.168.12.1:$DEPLOYDIR/;
+scp1 -oKexAlgorithms=+diffie-hellman-group1-sha1  "camUpdate/update_real.sh"  root@192.168.12.1:$DEPLOYDIR/;
+scp1 -oKexAlgorithms=+diffie-hellman-group1-sha1  "camUpdate/update_backend.sh"  root@192.168.12.1:$DEPLOYDIR/;
+scp1 -oKexAlgorithms=+diffie-hellman-group1-sha1  "camUpdate/update.md5sum"  root@192.168.12.1:$DEPLOYDIR/;
+scp1 -oKexAlgorithms=+diffie-hellman-group1-sha1  "camUpdate/update.sh"  root@192.168.12.1:$DEPLOYDIR/;
+scp1 -oKexAlgorithms=+diffie-hellman-group1-sha1  "camUpdate/Chronos1_4PowerController.X.production.hex"  root@192.168.12.1:$DEPLOYDIR/;
 echo "deployUpdate.sh finished"
